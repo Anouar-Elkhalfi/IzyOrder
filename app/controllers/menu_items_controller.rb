1ea2513be07cf89b9f829fec45f99f1b62a2ed51
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-05-29 10:34:33 +0200

    controllers

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,6 +1,9 @@
 class MenuItemsController < ApplicationController
   def index
     @menu_items = MenuItem.all
-    @qr_code = QrCodeService.generate_qr_code(menu_items_url)
+    # @qr_code = QrCodeService.generate_qr_code(menu_items_url)
+  end
+  def show
+    @menu_item = MenuItem.find(params[:id])
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,36 @@
 class OrdersController < ApplicationController
+
   def show
     @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = current_user.orders.build(order_params)
+    if @order.save
+      redirect_to @order, notice: 'Order was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to @order, notice: 'Order was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to orders_url, notice: 'Order was successfully destroyed.'
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status, :total_price, order_items_attributes: [:menu_item_id, :quantity])
   end
 end
